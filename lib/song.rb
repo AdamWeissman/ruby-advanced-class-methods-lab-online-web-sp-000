7e4583ebb1fe6496793af90c1081bf69ca84a760
Adam Weissman <adam@comedytragedyepic.com>
2019-09-24 08:41:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,23 @@
 class Song
-
+  
+  attr_accessor :name, :artist_name
   @@all = []
+ 
+  def self.all
+    @@all
+  end
+ 
+  def save
+    self.class.all << self
+  end
   
-  
-
   def self.create(a_song)
     @@all << a_song
   end
   
+ 
 end
+  
 
 
 =begin
