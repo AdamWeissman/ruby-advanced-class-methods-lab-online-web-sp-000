65b0b75edd8bf006089a64c7ec595db644737617
Adam Weissman <adam@comedytragedyepic.com>
2019-09-24 09:24:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
   end
   
   def self.new_from_filename(string_to_manipulate)
-    artist_name, song_name = string_to_manipulate.map.split(" - ")[0], 
+    artist_name, song_name = string_to_manipulate.map.split(" - ")[0]
     song_name = string_to_manipulate[1]
     
   end
